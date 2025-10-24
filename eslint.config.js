46893e64ff4771923dbb822fb6584405f1710419
Pradeep Nedunchezhiyan <p.nedunchezhiyan@walmart.com>
2025-10-24 22:10:03 +0530

    moved url to constant

--- eslint.config.js
+++ eslint.config.js
@@ -13,6 +13,14 @@ module.exports = [
     rules: {
       'prettier/prettier': 'error',
       'semi': ['warn', 'always'],
+          // Custom rule overrides or additions
+      "react/jsx-curly-brace-presence": ["error", { "props": "never", "children": "never" }],
+      "react/prop-types": "off", // Example: Turn off prop-types checking if using TypeScript
+      "no-irregular-whitespace": "error",
+      "indent": ["error", 2],
+      "no-trailing-spaces": "error",
+      "spaced-comment": ["error", "always"],
+      "key-spacing": ["error", { "beforeColon": false, "afterColon": true }]
     },
   },
 ];
